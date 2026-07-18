d2b86668a39e45f430d1750e305ef82cc96db870
Bableen Kaur <kaurbableen2006@gmail.com>
2026-07-19 01:22:25 +0530

    update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,41 +4,53 @@ const bcrypt =require('bcryptjs');
 const jwt= require('jsonwebtoken');
 
 
-async function registerUser(req,res){
-    const{fullName,email,password}= req.body;
+async function registerUser(req, res) {
+    try {
+        console.log("REGISTER API HIT");
+        console.log(req.body);
 
-    const isUserAlreadyExists= await userModel.findOne({
-        email
-    })
+        const { fullName, email, password } = req.body;
 
-    if(isUserAlreadyExists){
-        return res.status(400).json({
-            message:"User Already exists"
-        })
-    }
+        const isUserAlreadyExists = await userModel.findOne({ email });
 
-    const hashedPassword= await bcrypt.hash( password,10);
+        console.log("Existing:", isUserAlreadyExists);
 
-    const user =await userModel.create({
-        fullName,
-        email,
-        password:hashedPassword
-    })
-
-    const token=jwt.sign({
-        id:user._id,
-    },process.env.JWT_SECRET)
-    res.cookie("token",token)
-
-    res.status(201).json({
-        message:"User Registered Successfully",
-        user:{
-            _id:user._id,
-            email:user.email,
-            fullName:user.fullName
+        if (isUserAlreadyExists) {
+            return res.status(400).json({
+                message: "User Already exists"
+            });
         }
-    })
-    
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await userModel.create({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        console.log("USER CREATED:", user);
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET
+        );
+
+        res.cookie("token", token);
+
+        res.status(201).json({
+            message: "User Registered Successfully",
+            user
+        });
+
+    } catch (err) {
+        console.log("REGISTER ERROR:");
+        console.log(err);
+
+        return res.status(500).json({
+            error: err.message
+        });
+    }
 }
 
 async function loginUser(req,res){
